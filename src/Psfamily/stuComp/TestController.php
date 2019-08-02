e29c4a5b0e6aa62fd3ce451a97d245c6eb97f8d7
peijiyang <peijiyang@meishubao.com>
2019-08-02 11:50:51 +0800

    composer test

---
+++ src/Psfamily/stuComp/TestController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/8/2 0002
+ * Time: 11:43
+ */
+namespace Psfamily\stuComp;
+class TestController
+{
+    public static function hello()
+    {
+        echo "hello my composer.......";
+    }
+}
---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/8/2 0002
+ * Time: 11:46
+ */
+
+include "vendor/autoload.php";
+use Psfamily\stuComp\TestController;
+TestController::hello ();
